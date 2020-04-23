b7e0b48240ddcb7e9ac8d57bde97052777ef24d5
Kelsey Cahill <kelscahill@gmail.com>
2020-04-23 10:36:20 -0400

    Merge branch 'master' of github.com:adventistchurch/alps-wordpress-child

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # ALPS WordPress Child Theme
-The child theme enables `ALPS: Page Sections` custom fields to the `Front Page`.
 
 ## Theme installation via WordPress Admin Panel
 
@@ -11,14 +10,35 @@ The child theme enables `ALPS: Page Sections` custom fields to the `Front Page`.
 
 ## Theme settings
 
-### Setup for child theme
-2. Activate the child theme - `ALPS for Wordpress Child`
-3. Go to `Settings->Reading` and set a static page for the `Homepage`
-4. Save changes
-
 ### Requirements
 * The parent `ALPS for Wordpress` theme needs to be installed
 * The parent theme folder needs to be labeled `alps-wordpress-v3`
 * The child `ALPS for Wordpress Child` theme needs to be activated
 * The child theme folder needs to be labeled `alps-wordpress-v3-child`
-* A front page needs to be set in `Settings->Reading`
+
+### Child Theme structure
+Use this folder structure to override files in the parent theme.
+
+```shell
+themes/alps-wordpress-v3-child/
+├── dist/
+│   ├── images/
+│   ├── scripts/
+│   └── styles/
+└── views/
+    ├── layouts/
+    └── patterns/
+        ├── 00-atoms/
+        │   ├── icons/
+        │   └── logos/
+        ├── 01-molecules/
+        │   ├── blocks/
+        │   ├── components/
+        │   ├── forms/
+        │   └── navigation/
+        └── 02-organisms/
+            ├── asides/
+            ├── content/
+            ├── global/
+            └── sections/
+```
--- functions.php
+++ functions.php
@@ -3,127 +3,12 @@
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 function my_theme_enqueue_styles() {
 
-  $parent_style = 'parent-style'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
+  $parent_style = 'parent-style';
 
   wp_enqueue_style( $parent_style, get_template_directory_uri() . '/dist/styles/main.css' );
   wp_enqueue_style( 'child-style',
-    get_stylesheet_directory_uri() . '/style.css',
+    get_stylesheet_directory_uri() . '/dist/styles/main.css',
     array( $parent_style ),
     wp_get_theme()->get('Version')
   );
-}
-
-/**
- * Carbon fields
- */
-use Carbon_Fields\Container;
-use Carbon_Fields\Field;
-
-add_action('carbon_fields_register_fields', 'crb_attach_frontpage_fields');
-function crb_attach_frontpage_fields()
-{
-  Container::make('post_meta', __('ALPS: Page Sections', 'crb' ) )
-    ->where('post_id', '=', get_option('page_on_front'))
-    ->add_fields( array(
-      Field::make('complex', 'page_sections', 'Page Sections')
-        ->setup_labels( array(
-          'plural_name' => 'Page Sections',
-          'singular_name' => 'Page Section',
-        ) )
-
-        // Highlight Blocks
-        ->add_fields('highlight_blocks', 'Highlight Blocks', array(
-          Field::make('complex', 'highlight_block', 'Blocks')
-            ->setup_labels( array(
-              'plural_name' => 'Blocks',
-              'singular_name' => 'Block',
-            ) )
-            ->set_layout('tabbed-horizontal')
-            ->set_min('3')
-            ->set_max('3')
-            ->add_fields( array(
-              Field::make('text', 'highlight_block_description', 'Description'),
-            ) ),
-          Field::make('text', 'highlight_blocks_cta_text', __('Button Text'))->set_width(50)->set_attribute('placeholder', 'Learn More'),
-          Field::make('text', 'highlight_blocks_cta_url', __('Button URL'))->set_width(50)->set_attribute('placeholder', 'http://')
-        ) )
-
-        // Content Read More
-        ->add_fields('content_read_more', 'Content Read More', array(
-          Field::make('rich_text', 'content_read_more_body', 'Body')
-        ) )
-
-        // Split Content
-        ->add_fields('split_content', 'Split Content', array(
-          Field::make('complex', 'split_content_left', 'Left')
-            ->setup_labels( array(
-              'plural_name' => 'Content',
-              'singular_name' => 'Content',
-            ) )
-            ->set_width(50)
-            ->set_max('1')
-            ->add_fields( array(
-              Field::make('textarea', 'split_content_left_body', 'Body')
-                ->set_help_text('*Displays as large text')
-            ) ),
-          Field::make('complex', 'split_content_right', 'Right')
-            ->setup_labels( array(
-              'plural_name' => 'Content',
-              'singular_name' => 'Content',
-            ) )
-            ->set_width(50)
-            ->set_max('1')
-            ->add_fields( array(
-              Field::make('rich_text', 'split_content_right_body', 'Body'),
-              Field::make('text', 'split_content_right_cta_text', __('Button Text'))->set_width(50)->set_attribute('placeholder', 'Learn More'),
-              Field::make('text', 'split_content_right_cta_url', __('Button URL'))->set_width(50)->set_attribute('placeholder', 'http://')
-            ) ),
-        ) )
-
-        // Media Testimonies
-        ->add_fields('media_testimonies', 'Media Testimonies', array(
-          Field::make('text', 'media_testimonies_title', 'Title')->set_width(50),
-          Field::make('text', 'media_testimonies_url', 'See All Url')->set_width(50)->set_attribute('placeholder', 'http://'),
-          Field::make('complex', 'media_testimony', 'Testimonies')
-            ->setup_labels( array(
-              'plural_name' => 'Testimonies',
-              'singular_name' => 'Testimony',
-            ) )
-            ->set_width(50)
-            ->set_layout('tabbed-horizontal')
-            ->set_min('2')
-            ->add_fields( array(
-              Field::make('textarea', 'media_testimony_quote', 'Quote')->set_width(50),
-              Field::make('image', 'media_testimony_video_thumbnail', 'Video Thumbail' )->set_width(50),
-              Field::make('text', 'media_testimony_url', 'Read More Url')->set_attribute('placeholder', 'http://')->set_width(50),
-              Field::make('text', 'media_testimony_video_title', 'Video Title' )->set_width(50),
-            ) ),
-        ) )
-
-        // Home Body Content
-        ->add_fields('content_step_blocks', 'Content with Step Blocks', array(
-          Field::make('complex', 'content_step_blocks_content', 'Content')
-            ->setup_labels( array(
-              'plural_name' => 'Content',
-              'singular_name' => 'Content',
-            ) )
-            ->add_fields( array(
-              Field::make('rich_text', 'content_step_blocks_body', 'Body'),
-              Field::make('complex', 'step_blocks', 'Step Blocks')
-                ->setup_labels( array(
-                  'plural_name' => 'Blocks',
-                  'singular_name' => 'Block',
-                ) )
-                ->set_layout('tabbed-horizontal')
-                ->set_min('3')
-                ->set_max('3')
-                ->add_fields( array(
-                  Field::make('text', 'step_blocks_kicker', 'Kicker'),
-                  Field::make('text', 'step_blocks_text', 'Text'),
-                ) ),
-            ) ),
-            Field::make('text', 'content_step_blocks_cta_text', __('Button Text'))->set_width(50)->set_attribute('placeholder', 'Learn More'),
-            Field::make('text', 'content_step_blocks_cta_url', __('Button URL'))->set_width(50)->set_attribute('placeholder', 'http://')
-          ) )
-    ) );
 }
